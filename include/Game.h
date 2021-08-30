113c425d116146f0c4be8e509a6c85e50ed7aeb8
Thales Menezes <thlmenezes@pm.me>
2021-08-29 22:28:31 -0300

    feat: udpate constructor

--- include/Game.h
+++ include/Game.h
@@ -1,8 +1,11 @@
 #ifndef HEADER_GAME
 #define HEADER_GAME
+  #include <iostream>
+  using namespace std;
+
   class Game {
     static Game* instance;
-    Game();
+    Game(string title, int width, int height);
     public:
       static Game& GetInstance();
       ~Game();
