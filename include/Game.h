95360ef642da03769b52b040777009ca54a17006
Thales Menezes <thlmenezes@pm.me>
2021-08-30 17:29:27 -0300

    feat: update Game header

--- include/Game.h
+++ include/Game.h
@@ -1,10 +1,20 @@
 #ifndef HEADER_GAME
 #define HEADER_GAME
   #include <iostream>
+
   using namespace std;
+
+  #define INCLUDE_SDL_IMAGE
+  #define INCLUDE_SDL_MIXER
+  #include "SDL_include.h"
+  #include "State.h"
 
   class Game {
     static Game* instance;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    State* state;
+
     Game(string title, int width, int height);
     public:
       static Game& GetInstance();
