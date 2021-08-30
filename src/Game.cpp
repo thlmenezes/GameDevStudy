3c03839f0d109988184548143039d4c738a387ef
Thales Menezes <thlmenezes@pm.me>
2021-08-30 16:03:39 -0300

    feat: add sdl window and renderer to game

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,7 +8,8 @@ using namespace std;
 #define INCLUDE_SDL_MIXER
 #include "SDL_include.h"
 
-Game::Game(string title, int width, int height){
+Game::Game(string title, int width, int height)
+{
   if (instance != nullptr)
     throw_with_nested(runtime_error("Singleton Error"));
   instance = this;
@@ -17,43 +18,67 @@ Game::Game(string title, int width, int height){
     throw_with_nested(runtime_error(SDL_GetError()));
 
   const int sdl_image = IMG_Init(IMG_INIT_JPG);
-  if (0 == sdl_image){
+  if (0 == sdl_image)
+  {
     cout << sdl_image;
     throw_with_nested(runtime_error("SDL Image Init"));
   }
 
   const int sdl_mix = Mix_Init(MIX_INIT_OGG);
-  if (0 == sdl_mix){
+  if (0 == sdl_mix)
+  {
     cout << sdl_mix;
     throw_with_nested(runtime_error("MIX Audio Init"));
   }
 
   const int CHUNK_SIZE = 1024;
-  if(
-    Mix_OpenAudio(
-      MIX_DEFAULT_FREQUENCY,
-      MIX_DEFAULT_FORMAT,
-      MIX_DEFAULT_CHANNELS,
-      CHUNK_SIZE
-    )
-  )
+  if (
+      Mix_OpenAudio(
+          MIX_DEFAULT_FREQUENCY,
+          MIX_DEFAULT_FORMAT,
+          MIX_DEFAULT_CHANNELS,
+          CHUNK_SIZE))
     throw_with_nested(runtime_error("MIX Open Audio"));
 
   const int NUM_CHANNELS = 32;
   Mix_AllocateChannels(NUM_CHANNELS);
 
+  const Uint32 WINDOW_FLAGS = 0;
+  SDL_Window *game_window = SDL_CreateWindow(
+      title.c_str(),
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      width,
+      height,
+      WINDOW_FLAGS);
+
+  if (game_window == nullptr)
+    throw_with_nested(runtime_error("SDL Create Window"));
+
+  const int RENDERER_INDEX = -1;
+  SDL_Renderer *game_renderer = SDL_CreateRenderer(
+      game_window,
+      RENDERER_INDEX,
+      SDL_RENDERER_ACCELERATED);
+
+  if (game_renderer == nullptr)
+    throw_with_nested(runtime_error("SDL Create Renderer"));
+
   return;
 }
 
-Game& Game::GetInstance(){
+Game &Game::GetInstance()
+{
   const string TITLE = "Thales Lima Menezes - 170045919";
   const int WIDTH = 1024;
   const int HEIGHT = 600;
 
-  if (instance == nullptr) instance = new Game(TITLE,WIDTH,HEIGHT);
+  if (instance == nullptr)
+    instance = new Game(TITLE, WIDTH, HEIGHT);
 
   return *instance;
 }
 
-Game::~Game(){
+Game::~Game()
+{
 }
