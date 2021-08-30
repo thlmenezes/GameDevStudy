f3a85491fac718964d946547672b3631ca6354a6
Thales Menezes <thlmenezes@pm.me>
2021-08-30 17:28:40 -0300

    feat: create sprite header

---
+++ include/Sprite.h
@@ -0,0 +1,28 @@
+#ifndef HEADER_SPRITE
+#define HEADER_SPRITE
+  #include <iostream>
+
+  using namespace std;
+
+  #define INCLUDE_SDL_IMAGE
+  #include "SDL_include.h"
+
+  class Sprite{
+    SDL_Texture* texture;
+    int width;
+    int height;
+    SDL_Rect clipRect;
+
+    public:
+      Sprite();
+      Sprite(string file);
+      void Open(string file);
+      void SetClip(int x, int y,
+                  int w, int h);
+      void Render(int x, int y);
+      int GetWidth();
+      int GetHeight();
+      bool IsOpen();
+      ~Sprite();
+  };
+#endif
