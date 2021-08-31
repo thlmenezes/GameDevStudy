5c56476c602e97353fbb50ef958ef6a05f18b356
Thales Menezes <thlmenezes@pm.me>
2021-08-30 22:18:13 -0300

    feat: use argc/argv in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,11 @@
 #include "Game.h"
 
 Game *Game::instance = nullptr;
-int main(){
+int main(int argc, char **argv)
+{
+  Game &jogo = Game::GetInstance();
 
-  Game& jogo = Game::GetInstance();
+  // TODO: run game
 
   return 0;
 }
