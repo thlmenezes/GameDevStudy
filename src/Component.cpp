82ec9eecbc12d7efa64b282252cadd8cae8ea01c
Thales Menezes <thlmenezes@pm.me>
2021-09-16 03:21:41 -0300

    feat: add Component Implementation
    Althought abstract, there are some methods that require implementation

---
+++ src/Component.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+#include "Component.h"
+#include "GameObject.h"
+
+Component::Component(GameObject &associated) : associated(associated)
+{
+}
+
+Component::~Component()
+{
+}
