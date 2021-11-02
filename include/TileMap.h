b68ebfa344b7db17ae33140b1a14750f52bd0244
Thales Menezes <thlmenezes@pm.me>
2021-11-02 03:08:05 -0300

    feat(tilemap): create tilemap impl

---
+++ include/TileMap.h
@@ -0,0 +1,30 @@
+#ifndef HEADER_TILEMAP
+#define HEADER_TILEMAP
+  #include <iostream>
+
+  using namespace std;
+
+  #include "TileSet.h"
+  #include "GameObject.h"
+  #include "Component.h"
+
+  class TileMap: public Component {
+    vector<int> tileMatrix;
+    TileSet* tileSet;
+    int mapWidth, mapHeight, mapDepth;
+
+    public:
+      TileMap(GameObject& associated, string file, TileSet* tileSet);
+      void Load(string file);
+      void SetTileSet(TileSet* tileSet);
+      int& At(int x, int y, int z = 0);
+      void RenderLayer(int layer, int cameraX = 0, int cameraY = 0);
+      void Update(float dt);
+      void Render();
+      bool Is(string type);
+      int GetWidth();
+      int GetHeight();
+      int GetDepth();
+      ~TileMap();
+  };
+#endif
---
+++ src/TileMap.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <exception>
+#include <fstream>
+
+using namespace std;
+
+#include "TileMap.h"
+
+TileMap::TileMap(GameObject &associated, string file, TileSet *tileSet) : Component(associated)
+{
+  Load(file);
+  tileSet = tileSet;
+}
+
+void TileMap::Load(string file)
+{
+  ifstream rs(file, ios::in);
+
+  if (!rs.is_open())
+    throw_with_nested(runtime_error("Error opening file: " + file));
+
+  char c;
+
+  // # tileMap.txt
+  // mapWidth,mapHeight,mapDepth
+  rs >> mapWidth >> c >> mapHeight >> c >> mapDepth >> c;
+
+  int in;
+  while (rs >> in)
+  {
+    tileMatrix.push_back(in - 1);
+    rs >> c; // ignore char after number
+  }
+}
+
+void TileMap::SetTileSet(TileSet *tileSet)
+{
+  tileSet = tileSet;
+}
+
+int &TileMap::At(int x, int y, int z)
+{
+  int index = z * mapWidth * mapHeight + y * mapWidth + x;
+  return tileMatrix[index];
+}
+
+void TileMap::RenderLayer(int layer, int cameraX, int cameraY)
+{
+  for (int x = 0; x < mapWidth; x++)
+    for (int y = 0; y < mapHeight; y++)
+    {
+      int xPos = x * tileSet->GetTileWidth() + cameraX;
+      int yPos = y * tileSet->GetTileHeight() + cameraY;
+      tileSet->RenderTile(At(x, y, layer), xPos, yPos);
+    }
+}
+
+void TileMap::Update(float dt)
+{
+}
+
+void TileMap::Render()
+{
+  int cameraX = associated.box.x;
+  int cameraY = associated.box.y;
+
+  for (int layer = 0; layer < mapDepth; layer++)
+    RenderLayer(layer, cameraX, cameraY);
+}
+
+bool TileMap::Is(string type)
+{
+  return "TileMap" == type;
+}
+
+int TileMap::GetWidth()
+{
+  return mapWidth;
+}
+
+int TileMap::GetHeight()
+{
+  return mapHeight;
+}
+
+int TileMap::GetDepth()
+{
+  return mapDepth;
+}
+
+TileMap::~TileMap()
+{
+}
