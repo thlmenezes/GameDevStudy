b151fc5ce89b2114a54a4b14a81514a8bbc7a61f
Thales Menezes <thlmenezes@pm.me>
2021-11-09 01:22:50 -0300

    feat(camera follower): add impl

---
+++ include/CameraFollower.h
@@ -0,0 +1,19 @@
+#ifndef CAMERAFOLLOWER_H
+  #define CAMERAFOLLOWER_H
+
+  #include <string>
+
+  using namespace std;
+
+  #include "Component.h"
+
+  class CameraFollower : public Component {
+
+    public:
+      CameraFollower(GameObject& associated);
+
+      void Update(float dt);
+      void Render();
+      bool Is(string type);
+  };
+#endif
---
+++ src/CameraFollower.cpp
@@ -0,0 +1,24 @@
+// using namespace std;
+
+#include "CameraFollower.h"
+#include "Camera.h"
+
+CameraFollower::CameraFollower(GameObject &associated)
+    : Component(associated)
+{
+}
+
+void CameraFollower::Update(float dt)
+{
+  associated.box.x = Camera::pos.x;
+  associated.box.y = Camera::pos.y;
+}
+
+void CameraFollower::Render()
+{
+}
+
+bool CameraFollower::Is(string type)
+{
+  return "CameraFollower" == type;
+}
