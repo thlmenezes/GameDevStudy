32482878370c084ebc185fbd974909c17f3d95c8
Thales Menezes <thlmenezes@pm.me>
2021-11-15 03:23:52 -0300

    fix(component): start method error on compile
    may be a temporary fix

--- include/Component.h
+++ include/Component.h
@@ -10,7 +10,7 @@
       GameObject& associated;
     public:
       Component(GameObject& associated);
-      virtual void Start();
+      virtual void Start(){};
       virtual void Update(float dt) = 0;
       virtual void Render() = 0;
       virtual bool Is(string type) = 0;
