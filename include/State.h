17038a64cfac6932386f090cff4cefdf6df04044
Thales Menezes <thlmenezes@pm.me>
2022-05-14 04:14:20 -0300

    feat(state): generalize state

--- include/State.h
+++ include/State.h
@@ -1,34 +1,53 @@
 #ifndef STATE_H
   #define STATE_H
-  #include <vector>
   #include <memory>
+  #include <math.h>
 
   using namespace std;
 
-  #include "Music.h"
   #include "Sprite.h"
-  #include "GameObject.h"
+  #include "Music.h"
+  #include "Sound.h"
+  #include "InputManager.h"
+  #include "Camera.h" 
+  #include "CameraFollower.h"
+  #include "Alien.h"
+  #include "TileMap.h"
+  #include "PenguinBody.h"
+  
+  #include "Collider.h"
+  // #include "GameData.h"
 
-  class State {
-    bool quitRequested;
-    Music music;
-    Sprite* bg;
-    vector<shared_ptr<GameObject>> objectArray; 
-    bool started;
+  #define INCLUDE_SDL
+  #include "SDL_include.h"
 
-    void CollisionCheck();
+  class State{
+    Music *music;
+    void Input();
+
+    protected:
+      void StartArray();
+      virtual void UpdateArray(float dt);
+      virtual void RenderArray();
+      bool popRequested;
+      bool quitRequested;
+      bool started;
+      vector<shared_ptr<GameObject>> objectArray;
 
     public:
       State();
 
-      void LoadAssets();
-      void Start();
-      weak_ptr<GameObject> AddObject(GameObject* go);
-      weak_ptr<GameObject> GetObjectPtr(GameObject* go);
-      void Update(float dt);
-      void Render();
+      virtual void LoadAssets() = 0;
+      virtual void Update(float dt) = 0;
+      virtual void Render() = 0;
+      virtual void Start() = 0;
+      virtual void Pause () = 0;
+      virtual void Resume () = 0;
+      virtual weak_ptr<GameObject> AddObject(GameObject *go);
+      virtual weak_ptr<GameObject> GetObjectPtr(GameObject *go);
       bool QuitRequested();
+      bool PopRequested();
 
-      ~State();
+      virtual ~State();
   };
 #endif
--- src/State.cpp
+++ src/State.cpp
@@ -1,92 +1,45 @@
-#include <cmath>
-
-using namespace std;
-
 #include "State.h"
-#include "Sprite.h"
-#include "Vec2.h"
-#include "GameObject.h"
-#include "Game.h"
-#include "Sound.h"
-#include "Music.h"
-#include "TileSet.h"
-#include "TileMap.h"
-#include "InputManager.h"
-#include "Camera.h"
-#include "CameraFollower.h"
-#include "Alien.h"
-#include "PenguinBody.h"
-#include "Collider.h"
-#include "Collision.cpp"
-#define INCLUDE_SDL_IMAGE
-#define INCLUDE_SDL_MIXER
-#include "SDL_include.h"
-#define PI 3.14159265358979323846
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 600
 
 #define loop(x, n) for (long unsigned int x = 0; x < n; ++x)
 
-#define loopS(x, n, s) for (long unsigned int x = s; x < n; ++x)
-
 State::State()
     : quitRequested(false),
-      music("assets/audio/stageState.ogg"),
-      started(false)
+      started(false),
+      popRequested(false)
 {
 }
 
-void State::LoadAssets()
+void State::LoadAssets() {}
+
+void State::StartArray()
 {
-  music.Play(-1);
-  auto backgroundGO = new GameObject();
-  bg = new Sprite(*backgroundGO, "assets/img/ocean.jpg");
-  backgroundGO->AddComponent(bg);
-  backgroundGO->AddComponent(new CameraFollower(*backgroundGO));
-  AddObject(backgroundGO);
-
-  auto tilemapGO = new GameObject();
-  auto tilemap = new TileMap(
-      *tilemapGO,
-      "assets/map/tileMap.txt",
-      new TileSet(64, 64, "assets/img/tileset.png"));
-
-  tilemapGO->AddComponent(tilemap);
-  AddObject(tilemapGO);
-
-  auto alienGO = new GameObject();
-  auto alien = new Alien(*alienGO, 3);
-  alienGO->box.SetCenter(512, 300);
-  alienGO->AddComponent(alien);
-  AddObject(alienGO);
-
-  auto PenguinGo = new GameObject();
-  auto penguin = new PenguinBody(*PenguinGo);
-  PenguinGo->box.SetCenter(704, 640);
-  PenguinGo->AddComponent(penguin);
-  AddObject(PenguinGo);
-
-  Camera::Follow(PenguinGo);
-}
-
-void State::Start()
-{
-  LoadAssets();
-
   loop(i, objectArray.size())
   {
-    auto object = objectArray[i];
-    object->Start();
+    objectArray[i]->Start();
   }
+}
 
-  started = true;
+void State::UpdateArray(float dt)
+{
+  loop(i, objectArray.size())
+  {
+    objectArray[i]->Update(dt);
+  }
+}
+
+void State::RenderArray()
+{
+  loop(i, objectArray.size())
+  {
+    objectArray[i]->Render();
+  }
 }
 
 weak_ptr<GameObject> State::AddObject(GameObject *go)
 {
   shared_ptr<GameObject> shared = shared_ptr<GameObject>(go);
-  State::objectArray.push_back(shared);
-  if (State::started)
+  objectArray.push_back(shared);
+  if (started)
     shared->Start();
   return shared;
 }
@@ -99,61 +52,14 @@ weak_ptr<GameObject> State::GetObjectPtr(GameObject *go)
   return weak_ptr<GameObject>();
 }
 
-void State::Update(float dt)
-{
-  quitRequested = InputManager::GetInstance().QuitRequested() ||
-                  InputManager::GetInstance().KeyPress(ESCAPE_KEY);
-
-  Camera::Update(dt);
-
-  loop(i, objectArray.size())
-  {
-    auto object = objectArray[i];
-    object->Update(dt);
-    if (object->IsDead())
-    {
-      if (Camera::GetFocus() == object.get())
-        Camera::Unfollow();
-      objectArray.erase(objectArray.begin() + i);
-    }
-  }
-
-  CollisionCheck();
-}
-
-void State::CollisionCheck()
-{
-  loop(i, objectArray.size())
-  {
-    Collider *colliderI = (Collider *)objectArray[i]->GetComponent("Collider");
-    if (colliderI == nullptr)
-      continue;
-    loopS(j, objectArray.size(), i + 1)
-    {
-      Collider *colliderJ = (Collider *)objectArray[j]->GetComponent("Collider");
-      if (colliderJ == nullptr)
-        continue;
-
-      if (colliderI->IsColliding(*colliderJ))
-      {
-        objectArray[i]->NotifyCollision(*objectArray[j]);
-        objectArray[j]->NotifyCollision(*objectArray[i]);
-      }
-    }
-  }
-}
-
-void State::Render()
-{
-  for (auto &object : objectArray)
-  {
-    object->Render();
-  }
-}
-
 bool State::QuitRequested()
 {
   return quitRequested;
+}
+
+bool State::PopRequested()
+{
+  return popRequested;
 }
 
 State::~State()
