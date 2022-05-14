1161b5e29207500b36837f2a49694a8d9df0c73c
Thales Menezes <thlmenezes@pm.me>
2022-05-14 05:56:44 -0300

    feat(main): add title to game stack

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,12 @@
 #include "Game.h"
+#include "TitleState.h"
 
 Game *Game::instance = nullptr;
 int main(int argc, char **argv)
 {
   Game &jogo = Game::GetInstance();
-
+  State *newState = new TitleState();
+  jogo.Push(newState);
   jogo.Run();
 
   return 0;
